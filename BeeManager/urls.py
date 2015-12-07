ccc7d80931a44326b9595175041ef91c8ae8252a
AustinA <aadee92@gmail.com>
2015-12-06 20:46:23 -0800

    "Commit in NewBranchName"

--- BeeManager/urls.py
+++ BeeManager/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from rest_framework import routers
 from HiveManagement import views
 
+#A comment in new branch name
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
