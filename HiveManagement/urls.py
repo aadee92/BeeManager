2ce1cc8e23c9af98facdca0f9be1f0b16da4ff2d
AustinA <aadee92@gmail.com>
2015-12-06 20:26:11 -0800

    A test commit

--- HiveManagement/urls.py
+++ HiveManagement/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    #Some new comment
 
     #ex: /HiveManagement/
     url(r'^$', views.index, name='index'),
